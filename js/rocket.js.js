97e99750092ef58042b70f9acf38c5918489e8d8
vivianaAAA <antviviana17@gmail.com>
2022-11-11 02:15:30 +0200

    made it fly
    fixed the error and made the countdown

--- js/rocket.js.js
+++ js/rocket.js.js
@@ -4,12 +4,34 @@ var countDownNumber = 10;
 var changeState = function (state) {
   document.body.className = 'body-state'+
   state;
+   clearInterval(timer);
+   countDownNumber = 10;
+   document.getElementById('countDown').innerHTML = countDownNumber;
 
+   // countDown
   if (state == 2) {
     timer = setInterval(function(){
+       countDownNumber = countDownNumber -1;
       document.getElementById('countDown').innerHTML = countDownNumber;
-    countDownNumber = 9;
-    }, 1000);
+    
+    if (countDownNumber <= 0) {
+      changeState(3);
+    };
+    }, 900);
+  } else if (state == 3) {
+    var success = setTimeout(function ()
+    {
+          var randomNumber = Math.round(Math.random()*10);
+
+          console.log('randomNumber:', randomNumber)
+
+            // success
+          if (randomNumber > 5) {
+            changeState(4);
+          } else {
+            changeState(5); // oh noo
+          }
+    }, 2000);
   };
 
 }
